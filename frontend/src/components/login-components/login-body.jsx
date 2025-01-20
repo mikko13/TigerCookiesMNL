c9b27ca545c13dc098222b842744995774b0e581
mikko13 <mikko.samaniego.cics@ust.edu.ph>
2025-01-20 10:13:31 +0800

    Update login-body.jsx

--- frontend/src/components/login-components/login-body.jsx
+++ frontend/src/components/login-components/login-body.jsx
@@ -9,12 +9,12 @@ export default function LoginBody() {
   };
 
   return (
-    <main className="font-[sans-serif]">
+    <main className="font-[sans-serif] bg-[#fefee6]">
       <div className="mt-1 md:mt-[-70px] min-h-screen flex flex-col items-center justify-center py-6 px-4">
         <div className="grid md:grid-cols-2 items-center gap-6 max-w-6xl w-full">
-          <div className="border border-gray-300 rounded-lg p-6 max-w-md shadow-[0_2px_22px_-4px_rgba(93,96,127,0.2)] max-md:mx-auto">
+          <div className="border bg-[#ffffff] border-gray-300 rounded-lg p-6 max-w-md shadow-[0_2px_22px_-4px_rgba(93,96,127,0.2)] max-md:mx-auto">
             <form className="space-y-4">
-              <div className="mb-8">
+              <div className="mb-8 ">
                 <h3 className="text-gray-800 text-3xl font-bold">Sign in</h3>
                 <p className="text-gray-500 text-sm mt-4 leading-relaxed">
                   Sign in to your account
