ec355109e0cda05e3bff888b4e4402539d60f88a
mikko13 <mikko.samaniego.cics@ust.edu.ph>
2025-01-21 12:54:54 +0800

    Update adminManageAccount_Main.jsx

--- frontend/src/components/admin-manage-account-components/adminManageAccount_Main.jsx
+++ frontend/src/components/admin-manage-account-components/adminManageAccount_Main.jsx
@@ -6,10 +6,10 @@ export default function AdminManageAccountMain() {
       <div className="relative mx-4 mt-4 overflow-hidden text-gray-700 bg-white rounded-none bg-clip-border">
         <div className="flex flex-col justify-between gap-8 mb-4 md:flex-row md:items-center">
           <div>
-            <h5 className="block font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
+            <h5 className="block font-sans text-md md:text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
               Manage Employee Accounts
             </h5>
-            <p className="block mt-1 font-sans text-base antialiased font-normal leading-relaxed text-gray-700">
+            <p className="block mt-1 font-sans text-sm md:text-base antialiased font-normal leading-relaxed text-gray-700">
               These are details about the employees
             </p>
           </div>
@@ -43,7 +43,7 @@ export default function AdminManageAccountMain() {
               </div>
             </div>
             <button
-              className="flex select-none items-center gap-3 rounded-lg bg-yellow-400 hover:bg-yellow-500 py-2 px-4 text-center align-middle font-sans text-xs font-bold uppercase text-white transition-all focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none sm:py-1 sm:px-3 sm:text-xs"
+              className="flex select-none items-center gap-3 rounded-lg bg-yellow-400 hover:bg-yellow-500 py-1 px-2 md:py-2 md:px-4 text-center align-middle font-sans text-[10px] md:text-xs font-bold uppercase text-white transition-all focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none sm:py-1 sm:px-3 sm:text-xs"
               type="button"
             >
               Create Account
