a42672c3866d533a05a56b2dc3f2ece6620d16a7
BagBags <aaronbagain@gmail.com>
2025-01-21 20:16:42 +0800

    Edit

--- frontend/src/components/employee-attendance-components/empAttendance_checkOut.jsx
+++ frontend/src/components/employee-attendance-components/empAttendance_checkOut.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function EmpAttendanceCheckOut() {
   return (
     <div className="text-center">
-      {/* Submit Button */}
+      <h1 className="text-2xl font-semibold mb-6">Attendance</h1>
       <a href="./testing">
         <button
           type="button"
@@ -12,9 +12,7 @@ export default function EmpAttendanceCheckOut() {
           Check Out
         </button>
       </a>
-
-      {/* Back Button */}
-      <a href="./forgotpasswordotp">
+      <a href="./requestovertime">
         <button
           type="button"
           className="px-6 py-3 mt-2 w-full text-sm bg-gray-700 hover:bg-gray-800 text-white rounded-md active:bg-gray-900 focus:ring-2 focus:ring-gray-900 transition-all"
