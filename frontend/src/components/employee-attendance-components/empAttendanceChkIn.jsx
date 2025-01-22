142dfa1b3be27e295444c4cc2e3b1c2aa739ec8b
BagBags <aaronbagain@gmail.com>
2025-01-22 17:48:57 +0800

    Edit

---
+++ frontend/src/components/employee-attendance-components/empAttendanceChkIn.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import Sidebar from "./empAttendance_sidebar";
+import CheckIn from "./empAttendance_checkIn";
+import SidebarToggleButton from "./sidebar-toggle-button";
+
+export default function EmpAttendanceCheckIn() {
+  const [isSidebarVisible, setIsSidebarVisible] = useState(true);
+
+  const toggleSidebar = () => {
+    setIsSidebarVisible(!isSidebarVisible);
+  };
+  return (
+    <div className="flex h-screen">
+      <div
+        className={`transform ${
+          isSidebarVisible ? "translate-x-0" : "-translate-x-[250px]"
+        } transition-transform duration-300 fixed h-full z-10`}
+      >
+        <Sidebar />
+      </div>
+
+      <div
+        className={`absolute z-50 top-10 transform -translate-y-1/2 ${
+          isSidebarVisible ? "left-[240px]" : "-left-3"
+        } transition-all duration-300`}
+      >
+        <SidebarToggleButton
+          isSidebarVisible={isSidebarVisible}
+          toggleSidebar={toggleSidebar}
+        />
+      </div>
+
+      <div
+        className={`flex-grow flex items-center justify-center overflow-auto transition-all duration-300 ${
+          isSidebarVisible ? "ml-[250px]" : "ml-0"
+        }`}
+      >
+        <CheckIn />
+      </div>
+    </div>
+  );
+}
