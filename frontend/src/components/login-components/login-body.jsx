f6e94ac5d299423fa16ffc57b756826b0f638d2a
mikko13 <mikko.samaniego.cics@ust.edu.ph>
2025-01-27 09:34:32 +0800

    Added more components in login

--- frontend/src/components/login-components/login-body.jsx
+++ frontend/src/components/login-components/login-body.jsx
@@ -1,27 +1,9 @@
 import React from "react";
-import { useLoginState } from "./loginConstants";
-import { errorToast, successToast } from "./toastMessages";
-import sidepic from "../images/login-sidepic.svg";
-import "./ToastStyles.css";
+import LoginForm from "./login-form";
+import LoginSidePic from "./login-sidepic";
 import Background from "../images/background.png";
 
 export default function LoginBody() {
-  const {
-    email,
-    setEmail,
-    password,
-    setPassword,
-    showPassword,
-    setShowPassword,
-    error,
-    setError,
-    success,
-    setSuccess,
-    navigate,
-    togglePasswordVisibility,
-    handleLogin,
-  } = useLoginState();
-
   return (
     <main
       className="font-[sans-serif]"
@@ -33,139 +15,9 @@ export default function LoginBody() {
     >
       <div className="mt-1 md:mt-[-70px] min-h-screen flex flex-col items-center justify-center py-6 px-4">
         <div className="grid md:grid-cols-2 items-center gap-6 max-w-6xl w-full">
-          <div className="border bg-[#ffffff] border-gray-300 rounded-lg p-6 max-w-md shadow-[0_2px_22px_-4px_rgba(93,96,127,0.2)] max-md:mx-auto">
-            <form className="space-y-4" onSubmit={handleLogin}>
-              <div className="mb-8 ">
-                <h3 className="text-gray-800 text-3xl font-bold">Sign in</h3>
-                <p className="text-gray-500 text-sm mt-4 leading-relaxed">
-                  Sign in to your account
-                </p>
-              </div>
-
-              <div>
-                <label
-                  htmlFor="email"
-                  className="text-gray-800 text-sm mb-2 block"
-                >
-                  Email
-                </label>
-                <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  required
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  className="w-full text-sm text-gray-800 border border-gray-300 pl-4 pr-10 py-3 rounded-lg outline-blue-600"
-                  placeholder="Enter Email Address"
-                />
-              </div>
-
-              <div className="relative">
-                <label
-                  htmlFor="password"
-                  className="text-gray-800 text-sm mb-2 block"
-                >
-                  Password
-                </label>
-                <input
-                  id="password"
-                  name="password"
-                  type={showPassword ? "text" : "password"}
-                  required
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  className="w-full text-sm text-gray-800 border border-gray-300 pl-4 pr-10 py-3 rounded-lg outline-blue-600"
-                  placeholder="Enter password"
-                />
-                <button
-                  type="button"
-                  onClick={togglePasswordVisibility}
-                  className="absolute right-4 top-3 text-gray-500 hover:text-gray-700"
-                >
-                  {showPassword ? (
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="w-5 h-5 mt-7"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"
-                      />
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-                      />
-                    </svg>
-                  ) : (
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="w-5 h-5 mt-7"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88"
-                      />
-                    </svg>
-                  )}
-                </button>
-              </div>
-              <div className="flex flex-wrap items-center justify-between gap-4">
-                <div className="flex items-center">
-                  <input
-                    id="remember-me"
-                    name="remember-me"
-                    type="checkbox"
-                    className="h-4 w-4 shrink-0 text-blue-600 focus:ring-blue-500 border-gray-300 rounded"
-                  />
-                  <label
-                    htmlFor="remember-me"
-                    className="ml-3 block text-sm text-gray-800"
-                  >
-                    Remember me
-                  </label>
-                </div>
-
-                <div className="text-sm">
-                  <a
-                    href="./forgotpassword"
-                    className="text-yellow-400 hover:underline font-semibold"
-                  >
-                    Forgot your password?
-                  </a>
-                </div>
-              </div>
-              <button
-                type="submit"
-                className="w-full shadow-xl py-2.5 px-4 mt-3 text-sm tracking-wide rounded-lg text-white bg-yellow-400 hover:bg-yellow-500 focus:outline-none"
-              >
-                Sign in
-              </button>
-            </form>
-          </div>
-
-          <div className="max-md:mt-8 hidden md:flex">
-            <img
-              src={sidepic}
-              className="w-5/6 max-md:w-2/3 mx-auto block object-cover"
-              alt="Login"
-            />
-          </div>
+          <LoginForm />
+          <LoginSidePic />
         </div>
-
-        {error && errorToast}
-        {success && successToast}
       </div>
     </main>
   );
