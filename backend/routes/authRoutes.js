bafd523a5a08c3804cd4193fb5ba9d94232db65a
mikko13 <mikko.samaniego.cics@ust.edu.ph>
2025-02-08 22:15:25 +0800

    edited

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -8,6 +8,12 @@ const bcrypt = require("bcryptjs");
 
 const OAuth2 = google.auth.OAuth2;
 
+const oauth2Client = new OAuth2(
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET,
+  "https://developers.google.com/oauthplayground"
+);
+
 oauth2Client.setCredentials({ refresh_token: process.env.REFRESH_TOKEN });
 
 const sendMail = async (email, firstName, otp) => {
