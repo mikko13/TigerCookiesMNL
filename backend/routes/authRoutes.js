74782a6672c0afc5ab69d569df3c6d95aa79b2ce
mikko13 <mikko.samaniego.cics@ust.edu.ph>
2025-02-08 22:04:07 +0800

    Added Forgot Password Feature

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -8,11 +8,14 @@ const bcrypt = require("bcryptjs");
 
 const OAuth2 = google.auth.OAuth2;
 
+/*
 const oauth2Client = new OAuth2(
   process.env.CLIENT_ID,
   process.env.CLIENT_SECRET,
   "https://developers.google.com/oauthplayground"
 );
+
+*/
 
 oauth2Client.setCredentials({ refresh_token: process.env.REFRESH_TOKEN });
 
